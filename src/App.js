85d9959016f900ec7e615aceb47fb9494879eb78
DESKTOP-UG3PI5T\Novakovic <nikola2312017@gmail.com>
2022-03-06 15:37:48 +0100

    hero-section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Herosection from "./Components/Herosection";
 import Navbar from "./Components/Navbar";
 
 function App() {
   return (
     <div className="App">
         <Navbar />
+        <Herosection />
     </div>
   );
 }
---
+++ src/Components/Herosection.js
@@ -0,0 +1,98 @@
+import React from 'react'
+import '../Styles/Hero-section.scss'
+
+
+function Herosection() {
+  return (
+    <div className="hero-section">
+        <div className="overlay-hero"></div>
+
+        <div className="container">
+            <div className="left-hero">
+                <h2>Dé specialist…</h2>   
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p> 
+
+                <button><img src="images/white-shoping.svg" alt="" />Winkel</button>
+
+                <img src="images/left-arrrow.svg" alt="" />
+                <img src="images/right-arrrow.svg" alt="" />
+            </div>
+
+
+            <div className="right-hero">
+                <img className='window-hero-img' src="images/hero-window.png" alt="" />
+                <img className='waves-hero' src="images/waves.svg" alt="" />
+
+                <div className="content-right-hero">
+                    <h4>Dak</h4>    
+                    <h5>Lichtkoepels</h5>
+                    <p>De renovatie van bestaande koepels of het plaatsen van een nieuwe koepel ? Een traditionele…</p>
+                    <p className='view-btn-hero'>Klik hier voor meer informatie
+                    <img src="images/view-icon-right.svg" alt="" />
+                    </p>
+                </div>
+            </div>
+        </div>
+
+
+        <div className="footer-hero">
+            <div className="block-footer-main">
+                <span className="circle">
+                    <img src="images/box-icon.svg" alt="" />
+                </span>
+                <span className="content-block-footer">
+                    <p>Assortimente
+                    <img className='right-icon-white' src="images/right-icon.svg" alt="" />
+                    </p>
+                </span>
+            </div>
+
+            <div className="block-footer-main">
+                <span className="circle">
+                    <img src="images/message-icon.svg" alt="" />
+                </span>
+                <span className="content-block-footer">
+                    <p>Getuigenissen
+                    <img className='right-icon-white' src="images/right-icon.svg" alt="" />
+                    </p>
+                </span>
+            </div>
+
+            <div className="block-footer-main">
+                <span className="circle">
+                    <img src="images/reload-icon.svg" alt="" />
+                </span>
+                <span className="content-block-footer">
+                    <p>Up-To-Date
+                    <img className='right-icon-white' src="images/right-icon.svg" alt="" />
+                    </p>
+                </span>
+            </div>
+
+            <div className="block-footer-main">
+                <span className="circle">
+                    <img src="images/customer-support.svg" alt="" />
+                </span>
+                <span className="content-block-footer">
+                    <p>Services
+                    <img className='right-icon-white' src="images/right-icon.svg" alt="" />
+                    </p>
+                </span>
+            </div>
+
+            <div className="block-footer-main">
+                <span className="circle">
+                    <img src="images/wave-icon-small.svg" alt="" />
+                </span>
+                <span className="content-block-footer">
+                    <p>Wie zijn wij
+                    <img className='right-icon-white' src="images/right-icon.svg" alt="" />
+                    </p>
+                </span>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Herosection
